cee20d2f275aa4f167ef4b988cf9ec3ee24b91c9
AmatoFrancesco99 <francesco.amato01@universitadipavia.it>
2023-02-12 15:18:50 +0100

    Now the script generates a multiclass dataset, with parameters chosen by the user

--- Datasets/generate.py
+++ Datasets/generate.py
@@ -3,11 +3,22 @@
 # In this case the Iris classification dataset has been chosen.
 from sklearn import datasets, preprocessing
 from sklearn.model_selection import train_test_split
-import numpy as np
+import numpy as np, sys, pandas as pd
+
+if len(sys.argv) != 5:
+    print('Please pass all the needed parameters.')
+    exit()
+if sys.argv[1] == "-- help":
+    print('')
+else:
+    n_samples = int(sys.argv[1])
+    n_features = int(sys.argv[2])
+    n_classes = int(sys.argv[3])
+    split = int(sys.argv[4])
 
 # Load Iris dataset and divide in train & test
-dataset = datasets.load_iris()
-X_train, X_test, y_train, y_test = train_test_split(dataset.data, dataset.target, test_size=0.2)
+X, y = datasets.make_classification(n_samples=n_samples, n_features=n_features, n_informative=n_classes, n_classes=n_classes)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=(split/100))
 
 # Scaling numerical features
 minmax_scaler = preprocessing.MinMaxScaler().fit(X_train)
@@ -17,6 +28,9 @@ X_test = minmax_scaler.transform(X_test)
 # Store the datasets
 train_df = np.hstack((X_train, y_train[:,None]))
 test_df = np.hstack((X_test, y_test[:,None]))
-fmt = '%.1f', '%.1f', '%.1f', '%.1f', '%d'
+fmt = []
+for i in range(n_features):
+    fmt.append('%.1f')
+fmt.append('%d')
 np.savetxt(r'train.txt', train_df, fmt=fmt)
 np.savetxt(r'test.txt', test_df, fmt=fmt)
