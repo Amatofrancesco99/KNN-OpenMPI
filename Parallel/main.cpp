385a7755f35363404b0b8bbfc71bf99e7f252841
AmatoFrancesco99 <francesco.amato01@universitadipavia.it>
2023-02-12 19:20:36 +0100

    Added a file to consider how the execution time vary with respect to the number of samples and the number of overall slaves

--- Parallel/main.cpp
+++ Parallel/main.cpp
@@ -181,7 +181,17 @@ int main(int argc, char** argv) {
                 printf("%dNN Test accuracy: %.2f%%\n", K, ((sum_nodes_test_accuracies * n_processed_data) + weighted_reminder_accuracy) / N_TEST);
                 double endTime= MPI_Wtime();
                 double timeOfExecution = endTime - startTime;
+                // Print the execution time and write those details into a specific file
                 printf("\033[;32mExecution time: %.3fs \033[0m(%d overall samples, %d working nodes)\n", timeOfExecution, N_TRAIN + N_TEST, SIZE);
+                std::ofstream outfile;
+                outfile.open("execution-details.txt", std::ios_base::app); // append instead of overwrite
+                outfile << timeOfExecution;
+                outfile << " ";
+                outfile << N_TRAIN+N_TEST;
+                outfile << " ";
+                outfile << SIZE;
+                outfile << "\n";
+                outfile.close();
             }
 
             MPI_Finalize();
