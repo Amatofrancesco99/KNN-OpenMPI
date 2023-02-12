3cb2f19475920af999ac3f5a05a69590f0d03fcd
AmatoFrancesco99 <francesco.amato01@universitadipavia.it>
2023-02-12 16:16:20 +0100

    Improved the code for the datasets generation

--- Datasets/generate.py
+++ Datasets/generate.py
@@ -1,29 +1,28 @@
-# This simple snippet of Python code generates two sample files ('train.txt' & 'test.txt') used to check 
-# the C++ K-Nearest Neighbor Classifier implementation (serial/openMPI).
-# In this case the Iris classification dataset has been chosen.
+# This simple snippet of Python code generates two samples datasets ('train.txt' & 'test.txt') used to check 
+# the C++ K-Nearest Neighbor Classifier implementation (serial/openMPI), starting from some parameters passed by the user.
 from sklearn import datasets, preprocessing
 from sklearn.model_selection import train_test_split
-import numpy as np, sys, pandas as pd
+import numpy as np, sys, utils
 
-if len(sys.argv) != 5:
-    print('Please pass all the needed parameters.')
-    exit()
-if sys.argv[1] == "-- help":
-    print('')
+if len(sys.argv) != 6:
+    utils.print_description()
+    exit() 
 else:
     n_samples = int(sys.argv[1])
     n_features = int(sys.argv[2])
     n_classes = int(sys.argv[3])
     split = int(sys.argv[4])
+    apply_scaling = bool(int(sys.argv[5]))
 
-# Load Iris dataset and divide in train & test
+# Generate dataset and divide in train & test
 X, y = datasets.make_classification(n_samples=n_samples, n_features=n_features, n_informative=n_classes, n_classes=n_classes)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=(split/100))
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=(split/100))
 
 # Scaling numerical features
-minmax_scaler = preprocessing.MinMaxScaler().fit(X_train)
-X_train = minmax_scaler.transform(X_train)
-X_test = minmax_scaler.transform(X_test)
+if (apply_scaling == True):
+    minmax_scaler = preprocessing.MinMaxScaler().fit(X_train)
+    X_train = minmax_scaler.transform(X_train)
+    X_test = minmax_scaler.transform(X_test)
 
 # Store the datasets
 train_df = np.hstack((X_train, y_train[:,None]))
---
+++ Datasets/utils.py
@@ -0,0 +1,17 @@
+def print_description():
+    print("\n\033[1;33mK-NEAREST NEIGHBOR CLASSIFICATION ALGORITHM - GENERATE TRAIN & TEST DATASETS\033[0m\n\n"
+        "------------------------------------------------------------\n"
+        "\033[1;31mIt seems you did not pass all the required parameters to generate the train and test datasets. \033[0m\n\n"
+
+        "The following program, written in Python, allows to generate two general purpose multiclass datasets (already splitted into"
+        " train and test sets).\n\n"
+        "There are some arguments that must be passed to this program, in order to work properly, that are:\n"
+        "\t - Overall number of samples \033[;32m[n_samples]\033[0m\n"
+        "\t - Number of features for each sample \033[;32m[n_features]\033[0m\n"
+        "\t - Number of classes \033[;32m[n_classes]\033[0m\n"
+        "\t - Percentage of samples to put in the train set (remaining fraction into the test set) \033[;32m[split]\033[0m\n"
+        "\t - Apply MinMax scaling to avoid features with different value ranges \033[;32m[min-max_scaling]\033[0m\n"
+        "\n\033[1mRun it:\033[0m $ python3 generate.py \033[;32m[n_samples] [n_features] [n_classes] [split] [min-max_scaling]\033[0m\n\n\n"
+        "\033[;34mProgram developed by F. Amato & D. Ligari (Data Science track)\n"
+        "Advanced Computer Architecture project\n"
+        "University of Pavia - A.Y. 2022/23\033[0m\n\n");
