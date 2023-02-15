458cd103b34e32d8a37ddc5deec517b778cf1b57
AmatoFrancesco99 <francesco.amato01@universitadipavia.it>
2023-02-15 10:23:38 +0100

    Updated run tests using all vCPUs and fixed error in hostfile option

--- Parallel/Tests/GCP/run-tests.sh
+++ Parallel/Tests/GCP/run-tests.sh
@@ -10,72 +10,72 @@ select fav in "${cluster[@]}"; do
         "LIGHT")
             echo "\033[;32mStart running tests on LIGHT cluster\033[0m"
             # Using 1 CPUs and varying the datasets samples considered
-            mpirun -hostfile my_hosts -n 1 ../../main.o ../../../Datasets/train.txt 1000 ../../../Datasets/test.txt 200 6 50 4
-            mpirun -hostfile my_hosts -n 1 ../../main.o ../../../Datasets/train.txt 10000 ../../../Datasets/test.txt 2000 6 50 4
-            mpirun -hostfile my_hosts -n 1 ../../main.o ../../../Datasets/train.txt 50000 ../../../Datasets/test.txt 5000 6 50 4
+            mpirun -n 1 ../../main.o ../../../Datasets/train.txt 1000 ../../../Datasets/test.txt 200 6 50 4
+            mpirun -n 1 ../../main.o ../../../Datasets/train.txt 10000 ../../../Datasets/test.txt 2000 6 50 4
+            mpirun -n 1 ../../main.o ../../../Datasets/train.txt 50000 ../../../Datasets/test.txt 5000 6 50 4
 
-            # Using 2 CPUs and varying the datasets samples considered
-            mpirun -hostfile my_hosts -n 2 ../../main.o ../../../Datasets/train.txt 1000 ../../../Datasets/test.txt 200 6 50 4
-            mpirun -hostfile my_hosts -n 2 ../../main.o ../../../Datasets/train.txt 10000 ../../../Datasets/test.txt 2000 6 50 4
-            mpirun -hostfile my_hosts -n 2 ../../main.o ../../../Datasets/train.txt 50000 ../../../Datasets/test.txt 5000 6 50 4
+            # Using 2 vCPUs and varying the datasets samples considered
+            mpirun --use-hwthread-cpus --hostfile my_hosts -n 2 ../../main.o ../../../Datasets/train.txt 1000 ../../../Datasets/test.txt 200 6 50 4
+            mpirun --use-hwthread-cpus --hostfile my_hosts -n 2 ../../main.o ../../../Datasets/train.txt 10000 ../../../Datasets/test.txt 2000 6 50 4
+            mpirun --use-hwthread-cpus --hostfile my_hosts -n 2 ../../main.o ../../../Datasets/train.txt 50000 ../../../Datasets/test.txt 5000 6 50 4
 
-            # Using 4 CPUs and varying the datasets samples considered
-            mpirun -hostfile my_hosts -n 4 ../../main.o ../../../Datasets/train.txt 1000 ../../../Datasets/test.txt 200 6 50 4
-            mpirun -hostfile my_hosts -n 4 ../../main.o ../../../Datasets/train.txt 10000 ../../../Datasets/test.txt 2000 6 50 4
-            mpirun -hostfile my_hosts -n 4 ../../main.o ../../../Datasets/train.txt 50000 ../../../Datasets/test.txt 5000 6 50 4
+            # Using 4 vCPUs and varying the datasets samples considered
+            mpirun --use-hwthread-cpus --hostfile my_hosts -n 4 ../../main.o ../../../Datasets/train.txt 1000 ../../../Datasets/test.txt 200 6 50 4
+            mpirun --use-hwthread-cpus --hostfile my_hosts -n 4 ../../main.o ../../../Datasets/train.txt 10000 ../../../Datasets/test.txt 2000 6 50 4
+            mpirun --use-hwthread-cpus --hostfile my_hosts -n 4 ../../main.o ../../../Datasets/train.txt 50000 ../../../Datasets/test.txt 5000 6 50 4
 
-            # Using 8 CPUs and varying the datasets samples considered
-            mpirun -hostfile my_hosts -n 8 ../../main.o ../../../Datasets/train.txt 1000 ../../../Datasets/test.txt 200 6 50 4
-            mpirun -hostfile my_hosts -n 8 ../../main.o ../../../Datasets/train.txt 10000 ../../../Datasets/test.txt 2000 6 50 4
-            mpirun -hostfile my_hosts -n 8 ../../main.o ../../../Datasets/train.txt 50000 ../../../Datasets/test.txt 5000 6 50 4
+            # Using 8 vCPUs and varying the datasets samples considered
+            mpirun --use-hwthread-cpus --hostfile my_hosts -n 8 ../../main.o ../../../Datasets/train.txt 1000 ../../../Datasets/test.txt 200 6 50 4
+            mpirun --use-hwthread-cpus --hostfile my_hosts -n 8 ../../main.o ../../../Datasets/train.txt 10000 ../../../Datasets/test.txt 2000 6 50 4
+            mpirun --use-hwthread-cpus --hostfile my_hosts -n 8 ../../main.o ../../../Datasets/train.txt 50000 ../../../Datasets/test.txt 5000 6 50 4
 
-            # Using 10 CPUs and varying the datasets samples considered
-            mpirun -hostfile my_hosts -n 10 ../../main.o ../../../Datasets/train.txt 1000 ../../../Datasets/test.txt 200 6 50 4
-            mpirun -hostfile my_hosts -n 10 ../../main.o ../../../Datasets/train.txt 10000 ../../../Datasets/test.txt 2000 6 50 4
-            mpirun -hostfile my_hosts -n 10 ../../main.o ../../../Datasets/train.txt 50000 ../../../Datasets/test.txt 5000 6 50 4
+            # Using 10 vCPUs and varying the datasets samples considered
+            mpirun --use-hwthread-cpus --hostfile my_hosts -n 10 ../../main.o ../../../Datasets/train.txt 1000 ../../../Datasets/test.txt 200 6 50 4
+            mpirun --use-hwthread-cpus --hostfile my_hosts -n 10 ../../main.o ../../../Datasets/train.txt 10000 ../../../Datasets/test.txt 2000 6 50 4
+            mpirun --use-hwthread-cpus --hostfile my_hosts -n 10 ../../main.o ../../../Datasets/train.txt 50000 ../../../Datasets/test.txt 5000 6 50 4
 
-            # Using 12 CPUs and varying the datasets samples considered
-            mpirun -hostfile my_hosts -n 12 ../../main.o ../../../Datasets/train.txt 1000 ../../../Datasets/test.txt 200 6 50 4
-            mpirun -hostfile my_hosts -n 12 ../../main.o ../../../Datasets/train.txt 10000 ../../../Datasets/test.txt 2000 6 50 4
-            mpirun -hostfile my_hosts -n 12 ../../main.o ../../../Datasets/train.txt 50000 ../../../Datasets/test.txt 5000 6 50 4
+            # Using 12 vCPUs and varying the datasets samples considered
+            mpirun --use-hwthread-cpus --hostfile my_hosts -n 12 ../../main.o ../../../Datasets/train.txt 1000 ../../../Datasets/test.txt 200 6 50 4
+            mpirun --use-hwthread-cpus --hostfile my_hosts -n 12 ../../main.o ../../../Datasets/train.txt 10000 ../../../Datasets/test.txt 2000 6 50 4
+            mpirun --use-hwthread-cpus --hostfile my_hosts -n 12 ../../main.o ../../../Datasets/train.txt 50000 ../../../Datasets/test.txt 5000 6 50 4
             echo "\033[;32mEnded tests on LIGHT cluster\033[0m"
             ;;
         "FAT")
             echo "\033[;34mStart running tests on FAT cluster\033[0m"
 	        # Using 1 CPUs and varying the datasets samples considered
-            mpirun -hostfile my_hosts -n 1 ../../main.o ../../../Datasets/train.txt 1000 ../../../Datasets/test.txt 200 6 50 4
-            mpirun -hostfile my_hosts -n 1 ../../main.o ../../../Datasets/train.txt 10000 ../../../Datasets/test.txt 2000 6 50 4
-            mpirun -hostfile my_hosts -n 1 ../../main.o ../../../Datasets/train.txt 50000 ../../../Datasets/test.txt 5000 6 50 4
+            mpirun -n 1 ../../main.o ../../../Datasets/train.txt 1000 ../../../Datasets/test.txt 200 6 50 4
+            mpirun -n 1 ../../main.o ../../../Datasets/train.txt 10000 ../../../Datasets/test.txt 2000 6 50 4
+            mpirun -n 1 ../../main.o ../../../Datasets/train.txt 50000 ../../../Datasets/test.txt 5000 6 50 4
 
-            # Using 4 CPUs and varying the datasets samples considered
-            mpirun -hostfile my_hosts -n 4 ../../main.o ../../../Datasets/train.txt 1000 ../../../Datasets/test.txt 200 6 50 4
-            mpirun -hostfile my_hosts -n 4 ../../main.o ../../../Datasets/train.txt 10000 ../../../Datasets/test.txt 2000 6 50 4
-            mpirun -hostfile my_hosts -n 4 ../../main.o ../../../Datasets/train.txt 50000 ../../../Datasets/test.txt 5000 6 50 4
+            # Using 4 vCPUs and varying the datasets samples considered
+            mpirun --use-hwthread-cpus --hostfile my_hosts -n 4 ../../main.o ../../../Datasets/train.txt 1000 ../../../Datasets/test.txt 200 6 50 4
+            mpirun --use-hwthread-cpus --hostfile my_hosts -n 4 ../../main.o ../../../Datasets/train.txt 10000 ../../../Datasets/test.txt 2000 6 50 4
+            mpirun --use-hwthread-cpus --hostfile my_hosts -n 4 ../../main.o ../../../Datasets/train.txt 50000 ../../../Datasets/test.txt 5000 6 50 4
 
-            # Using 8 CPUs and varying the datasets samples considered
-            mpirun -hostfile my_hosts -n 8 ../../main.o ../../../Datasets/train.txt 1000 ../../../Datasets/test.txt 200 6 50 4
-            mpirun -hostfile my_hosts -n 8 ../../main.o ../../../Datasets/train.txt 10000 ../../../Datasets/test.txt 2000 6 50 4
-            mpirun -hostfile my_hosts -n 8 ../../main.o ../../../Datasets/train.txt 50000 ../../../Datasets/test.txt 5000 6 50 4
+            # Using 8 vCPUs and varying the datasets samples considered
+            mpirun --use-hwthread-cpus --hostfile my_hosts -n 8 ../../main.o ../../../Datasets/train.txt 1000 ../../../Datasets/test.txt 200 6 50 4
+            mpirun --use-hwthread-cpus --hostfile my_hosts -n 8 ../../main.o ../../../Datasets/train.txt 10000 ../../../Datasets/test.txt 2000 6 50 4
+            mpirun --use-hwthread-cpus --hostfile my_hosts -n 8 ../../main.o ../../../Datasets/train.txt 50000 ../../../Datasets/test.txt 5000 6 50 4
             
-            # Using 12 CPUs and varying the datasets samples considered
-            mpirun -hostfile my_hosts -n 12 ../../main.o ../../../Datasets/train.txt 1000 ../../../Datasets/test.txt 200 6 50 4
-            mpirun -hostfile my_hosts -n 12 ../../main.o ../../../Datasets/train.txt 10000 ../../../Datasets/test.txt 2000 6 50 4
-            mpirun -hostfile my_hosts -n 12 ../../main.o ../../../Datasets/train.txt 50000 ../../../Datasets/test.txt 5000 6 50 4
+            # Using 12 vCPUs and varying the datasets samples considered
+            mpirun --use-hwthread-cpus --hostfile my_hosts -n 12 ../../main.o ../../../Datasets/train.txt 1000 ../../../Datasets/test.txt 200 6 50 4
+            mpirun --use-hwthread-cpus --hostfile my_hosts -n 12 ../../main.o ../../../Datasets/train.txt 10000 ../../../Datasets/test.txt 2000 6 50 4
+            mpirun --use-hwthread-cpus --hostfile my_hosts -n 12 ../../main.o ../../../Datasets/train.txt 50000 ../../../Datasets/test.txt 5000 6 50 4
 
-            # Using 16 CPUs and varying the datasets samples considered
-            mpirun -hostfile my_hosts -n 16 ../../main.o ../../../Datasets/train.txt 1000 ../../../Datasets/test.txt 200 6 50 4
-            mpirun -hostfile my_hosts -n 16 ../../main.o ../../../Datasets/train.txt 10000 ../../../Datasets/test.txt 2000 6 50 4
-            mpirun -hostfile my_hosts -n 16 ../../main.o ../../../Datasets/train.txt 50000 ../../../Datasets/test.txt 5000 6 50 4
+            # Using 16 vCPUs and varying the datasets samples considered
+            mpirun --use-hwthread-cpus --hostfile my_hosts -n 16 ../../main.o ../../../Datasets/train.txt 1000 ../../../Datasets/test.txt 200 6 50 4
+            mpirun --use-hwthread-cpus --hostfile my_hosts -n 16 ../../main.o ../../../Datasets/train.txt 10000 ../../../Datasets/test.txt 2000 6 50 4
+            mpirun --use-hwthread-cpus --hostfile my_hosts -n 16 ../../main.o ../../../Datasets/train.txt 50000 ../../../Datasets/test.txt 5000 6 50 4
 
-            # Using 20 CPUs and varying the datasets samples considered
-            mpirun -hostfile my_hosts -n 20 ../../main.o ../../../Datasets/train.txt 1000 ../../../Datasets/test.txt 200 6 50 4
-            mpirun -hostfile my_hosts -n 20 ../../main.o ../../../Datasets/train.txt 10000 ../../../Datasets/test.txt 2000 6 50 4
-            mpirun -hostfile my_hosts -n 20 ../../main.o ../../../Datasets/train.txt 50000 ../../../Datasets/test.txt 5000 6 50 4
+            # Using 20 vCPUs and varying the datasets samples considered
+            mpirun --use-hwthread-cpus --hostfile my_hosts -n 20 ../../main.o ../../../Datasets/train.txt 1000 ../../../Datasets/test.txt 200 6 50 4
+            mpirun --use-hwthread-cpus --hostfile my_hosts -n 20 ../../main.o ../../../Datasets/train.txt 10000 ../../../Datasets/test.txt 2000 6 50 4
+            mpirun --use-hwthread-cpus --hostfile my_hosts -n 20 ../../main.o ../../../Datasets/train.txt 50000 ../../../Datasets/test.txt 5000 6 50 4
 
-            # Using 24 CPUs and varying the datasets samples considered
-            mpirun -hostfile my_hosts -n 24 ../../main.o ../../../Datasets/train.txt 1000 ../../../Datasets/test.txt 200 6 50 4
-            mpirun -hostfile my_hosts -n 24 ../../main.o ../../../Datasets/train.txt 10000 ../../../Datasets/test.txt 2000 6 50 4
-            mpirun -hostfile my_hosts -n 24 ../../main.o ../../../Datasets/train.txt 50000 ../../../Datasets/test.txt 5000 6 50 4
+            # Using 24 vCPUs and varying the datasets samples considered
+            mpirun --use-hwthread-cpus --hostfile my_hosts -n 24 ../../main.o ../../../Datasets/train.txt 1000 ../../../Datasets/test.txt 200 6 50 4
+            mpirun --use-hwthread-cpus --hostfile my_hosts -n 24 ../../main.o ../../../Datasets/train.txt 10000 ../../../Datasets/test.txt 2000 6 50 4
+            mpirun --use-hwthread-cpus --hostfile my_hosts -n 24 ../../main.o ../../../Datasets/train.txt 50000 ../../../Datasets/test.txt 5000 6 50 4
             echo "\033[;34mEnded tests on FAT cluster\033[0m"
 	        break
             ;;
